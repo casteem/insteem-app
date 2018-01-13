6b5eb9ed29f0b5bd9b7dc3d9b86ca071de40e9e9
sarasate <luis@superlumen.io>
2018-01-13 22:28:55 +0100

    Add styled-components

--- src/components/StoryList/components/StoryListItem.js
+++ src/components/StoryList/components/StoryListItem.js
@@ -1,12 +1,17 @@
 import React from "react";
+import styled from "styled-components/native";
 import { View, Text } from "react-native";
+
+const Item = styled.View`
+  padding: 12px;
+`;
 
 const StoryListItem = props => {
   const { story } = props;
   return (
-    <View>
+    <Item>
       <Text>{story.title}</Text>
-    </View>
+    </Item>
   );
 };
 
