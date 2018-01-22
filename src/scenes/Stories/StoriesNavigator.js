64736abc97c3eafe889cbdb590b451ec926902a6
Luis Höfer <luis@superlumen.io>
2018-01-22 13:17:30 +0100

    Fix tab icon for StoriesScene

--- src/scenes/Stories/StoriesNavigator.js
+++ src/scenes/Stories/StoriesNavigator.js
@@ -10,6 +10,9 @@ const StoriesNavigator = StackNavigator({
 });
 
 StoriesNavigator.navigationOptions = {
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="ios-list" color={tintColor} size={30} />
+  )
   // swipeEnabled: true
 };
 
