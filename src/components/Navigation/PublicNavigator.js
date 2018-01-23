2660647e6793605a2697b7908b2d08f1d3017bbd
sarasate <luis@superlumen.io>
2018-01-23 12:44:55 +0100

    Enable StoryScene in public view

--- src/components/Navigation/PublicNavigator.js
+++ src/components/Navigation/PublicNavigator.js
@@ -2,13 +2,16 @@ import React from "react";
 import { StackNavigator } from "react-navigation";
 import Public from "../../scenes/Public";
 import SigninScene from "../../scenes/Auth/SigninScene";
+import StoryScene from "../../scenes/Stories/StoryScene";
 
 // The main navigation of the app will be TabNavigator.
 
 const PublicNavigator = StackNavigator(
   {
     Home: { screen: Public },
-    Signin: { screen: SigninScene, path: "signin" }
+    Signin: { screen: SigninScene, path: "signin" },
+    Story: { screen: StoryScene, path: "stories/:story" }
+
     // New: { screen: SubmitNavigator },
     // Votes: { screen: FollowerNavigator },
     // Profile: { screen: ProfileNavigator }
